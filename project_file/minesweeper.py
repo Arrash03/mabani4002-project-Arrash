91bad2de25ec3e05017bbe748a28ffa99e7c4126
Arrash <atv1382@gmail.com>
2022-06-30 10:53:27 +0430

    parts of main window.

--- project_file/minesweeper.py
+++ project_file/minesweeper.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from settings import Width, Height
+from utils import width_percent, height_percent
 
 window = Tk()
 # base info
@@ -8,6 +9,15 @@ window.resizable(False, False)
 window.geometry(f"{Width}x{Height}")
 window.config(bg="black")
 
+# main part of window
+frame1 = Frame(window, width=width_percent(15), height=Height, bg="black")
+frame1.place(x=0, y=0)
+
+frame2 = Frame(window, width=width_percent(70), height=Height, bg="black")
+frame2.place(x=width_percent(15), y=0)
+
+frame3 = Frame(window, width=width_percent(15), height=Height, bg="black")
+frame3.place(x=width_percent(85), y=0)
 
 
 window.mainloop()
---
+++ project_file/utils.py
@@ -0,0 +1,8 @@
+from settings import Width, Height
+
+def width_percent(percentage):
+    return Width*percentage/100
+
+def height_percent(percentage):
+    return Height*percentage/100
+
