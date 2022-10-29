3a7e08267eb97e471a3c612cd1d086d4373820d7
Arrash <atv1382@gmail.com>
2022-10-29 13:46:06 +0330

    upgrade UI

--- project_file/cell.py
+++ project_file/cell.py
@@ -1,15 +1,17 @@
-from tkinter import *
-from tkinter import messagebox
+from tkinter import messagebox, Button, DISABLED, Label
+import tkinter.font as font
+from time import sleep
 from random import sample
 from settings import mine_number, Gridsize_width, Gridsize_height
 from utils import height_percent
-# import ctypes
+
+game_frame = None
 
 class Cell:
     mouse_click = 1
     flag_number = mine_number
     flag_lbl = None
-    cell_number = Gridsize_width*Gridsize_height
+    cell_number = Gridsize_width * Gridsize_height
     cell_lbl = None
 
     all_cell = []
@@ -25,17 +27,17 @@ class Cell:
 
         self.cell_btn_object = None
 
-
     def createcell(self, frame):
-        btn = Button(frame, text="", width=4, height=2)
+        global game_frame
+        game_frame = frame
+        btn = Button(frame, text="", width=4, height=2, font=font.Font(family="Cabinet Grotesk", size=10, weight="bold"))
         btn.bind("<Button-1>", self.left_click)
-        btn.bind("<Button-3>", self.right_click)
         self.cell_btn_object = btn
         self.cell_btn_object.grid(column=self.i, row=self.j, padx=1, pady=1)
 
     def randomize_mines(self):
-        un_mine_cells=[self.surrounded_cells(self.i+i, self.j+j) for i in [-1, 0, 1] for j in [-1, 0, 1]
-                       if self.surrounded_cells(self.i+i, self.j+j)!= None]
+        un_mine_cells = [self.surrounded_cells(self.i + i, self.j + j) for i in [-1, 0, 1] for j in [-1, 0, 1]
+                         if self.surrounded_cells(self.i + i, self.j + j) is not None]
         candidate_cells = [cell for cell in Cell.all_cell if cell not in un_mine_cells]
         mines_li = sample(candidate_cells, 31)
         for mine in mines_li:
@@ -43,15 +45,15 @@ class Cell:
 
     def show_cell(self):
         counter = 0
-        surround = [self.surrounded_cells(self.i+i, self.j+j) for i in [-1, 0, 1] for j in [-1, 0, 1]
-                    if self.surrounded_cells(self.i+i, self.j+j)!=None]
+        surround = [self.surrounded_cells(self.i + i, self.j + j) for i in [-1, 0, 1] for j in [-1, 0, 1]
+                    if self.surrounded_cells(self.i + i, self.j + j) != None]
         for surr_cell in surround:
             if surr_cell.is_mine:
                 counter += 1
-        if counter!=0:
+        if counter != 0:
             self.cell_btn_object.config(text=f"{counter}")
         else:
-            self.cell_btn_object.config(bg="burlywood")
+            self.cell_btn_object.config(bg="#FDAE38")
             # self.empty_cell()
         self.is_opened = True
         self.cell_btn_object.config(state=DISABLED)
@@ -59,20 +61,24 @@ class Cell:
         self.cell_btn_object.unbind("<Button-3>")
 
     def show_mine(self):
-        self.cell_btn_object.config(text="mine", bg="red", fg="white")
+        self.cell_btn_object.config(state=DISABLED)
+        self.cell_btn_object.config(text="\U0001F4A3", bg="#FD5B5F")
         for cell in Cell.all_cell:
             cell.cell_btn_object.config(state=DISABLED)
             cell.cell_btn_object.unbind("<Button-1>")
             cell.cell_btn_object.unbind("<Button-3>")
             if cell.is_mine:
-                cell.cell_btn_object.config(text="mine", bg="red", fg="white")
-        # ctypes.windll.user32.MessageBoxW(0, "You clicked on a Mine!!", "Game Over", 0)
-        messagebox.showinfo(message="You clicked on a Mine!!", title="Game Over")
+                cell.cell_btn_object.config(text="\U0001F4A3", bg="#FD5B5F")
+                sleep(0.1)
+                game_frame.update()
+        sleep(0.5)
+        messagebox.showwarning(message="You clicked on a Mine!!", title="Game Over")
+
 
     @staticmethod
     def surrounded_cells(x, y):
         for cell in Cell.all_cell:
-            if cell.i==x and cell.j==y:
+            if cell.i == x and cell.j == y:
                 return cell
 
     def left_click(self, event):
@@ -87,7 +93,6 @@ class Cell:
                     self.empty_cell()
                 if Cell.cell_number == mine_number:
                     messagebox.showinfo(title="Win", message="Congratulations!!You Won the Game!!")
-                    # ctypes.windll.user32.MessageBoxW(0, "Congratulations!!You Won the Game!!", "The End", 0)
         else:
             self.first_click()
             Cell.cell_number -= 1
@@ -95,32 +100,38 @@ class Cell:
 
     def right_click(self, event):
         if not self.is_flag:
-            if Cell.flag_number>0:
+            if Cell.flag_number > 0:
                 self.is_flag = True
                 Cell.flag_number -= 1
                 Cell.flag_lbl.config(text=f"Flag : {Cell.flag_number}")
-                self.cell_btn_object.config(text="F", bg="orange", fg="white", state=DISABLED)
+                self.cell_btn_object.config(text="\U0001F3F3", bg="#7CB3D0", fg="white", state=DISABLED)
                 self.cell_btn_object.unbind("<Button-1>")
         else:
             self.is_flag = False
             Cell.flag_number += 1
             Cell.flag_lbl.config(text=f"Flag : {Cell.flag_number}")
-            self.cell_btn_object.config(text="", bg="SystemButtonFace", fg="black", state=NORMAL)
+            self.cell_btn_object.config(text="", bg="SystemButtonFace", fg="black", state=font.NORMAL)
             self.cell_btn_object.bind("<Button-1>", self.left_click)
 
     def first_click(self):
         self.randomize_mines()
         self.show_cell()
+        #
+        for btn in Cell.all_cell:
+            btn.cell_btn_object.bind("<Button-3>", btn.right_click)
+        self.cell_btn_object.unbind("<Button-3>")
+        #
         self.empty_cell()
         Cell.mouse_click += 1
+
 
     def empty_cell(self):
         picked_cells = [self]
         while picked_cells != []:
             for i in [-1, 0, 1]:
                 for j in [-1, 0, 1]:
-                    if i!=0 or j!=0:
-                        picked_cell = picked_cells[0].surrounded_cells(picked_cells[0].i+i, picked_cells[0].j+j)
+                    if i != 0 or j != 0:
+                        picked_cell = picked_cells[0].surrounded_cells(picked_cells[0].i + i, picked_cells[0].j + j)
                         if picked_cell != None and not picked_cell.is_opened:
                             picked_cell.show_cell()
                             Cell.cell_number -= 1
@@ -129,16 +140,16 @@ class Cell:
                                 picked_cells.append(picked_cell)
             picked_cells.pop(0)
 
-
     @classmethod
     def create_lbl(cls, frame):
-        Cell.cell_lbl = Label(frame, text=f"Cell : {Gridsize_width*Gridsize_height}", bg="#18191a", fg="burlywood", font=("Times New Roman", 15))
+        Cell.cell_lbl = Label(frame, text=f"Cell : {Gridsize_width * Gridsize_height}", bg="#264248", fg="#FDAE38",
+                              font=("Cabinet Grotesk", 15))
         Cell.cell_lbl.place(x=5, y=height_percent(10))
         Cell.cell_number = Gridsize_width * Gridsize_height
-        Cell.flag_lbl = Label(frame, text=f"Flag : {mine_number}", bg="#18191a", fg="burlywood", font=("Times New Roman", 15))
+        Cell.flag_lbl = Label(frame, text=f"Flag : {mine_number}", bg="#264248", fg="#FDAE38",
+                              font=("Cabinet Grotesk", 15))
         Cell.flag_lbl.place(x=8, y=height_percent(18))
         Cell.flag_number = mine_number
 
     def __repr__(self):
         return f"cell({self.i}, {self.j})"
-
--- project_file/minesweeper.py
+++ project_file/minesweeper.py
@@ -1,3 +1,4 @@
+from time import sleep
 from tkinter import *
 from cell import Cell
 from settings import Width, Height, Gridsize_width, Gridsize_height
@@ -8,19 +9,19 @@ window = Tk()
 window.title("Minesweeper Game")
 window.resizable(False, False)
 window.geometry(f"{Width}x{Height}")
-window.config(bg="black")
+window.config(bg="#4FA3A5")
 
 # main part of window
-frame1 = Frame(window, width=width_percent(15), height=Height, bg="#18191a")
+frame1 = Frame(window, width=width_percent(15), height=Height, bg="#264248")
 frame1.place(x=0, y=0)
 
-frame2 = Frame(window, width=width_percent(70), height=Height, bg="#1f2123")
+frame2 = Frame(window, width=width_percent(70), height=Height, bg="#4FA3A5")
 frame2.place(x=width_percent(15), y=0)
 
-frame2_1 = Frame(frame2, width=width_percent(70), height=Height, bg="burlywood")
+frame2_1 = Frame(frame2, width=width_percent(70), height=Height, bg="#FDAE38")
 frame2_1.grid(padx=18, pady=6)
 
-frame3 = Frame(window, width=width_percent(15), height=Height, bg="#18191a")
+frame3 = Frame(window, width=width_percent(15), height=Height, bg="#264248")
 frame3.place(x=width_percent(85), y=0)
 
 # creating cells
@@ -28,6 +29,7 @@ for i in range(Gridsize_width):
     for j in range(Gridsize_height):
         c = Cell(i, j)
         c.createcell(frame2_1)
+        frame2_1.update()
 
 # frame1
 Cell.create_lbl(frame1)
@@ -41,10 +43,14 @@ def reset():
         cell.is_mine = False
         cell.is_flag = False
         cell.createcell(frame2_1)
+        window.update()
     Cell.create_lbl(frame1)
+    sleep(0.01)
+    window.update()
 
-Button(frame3, text="Play Again", command=reset, bg="burlywood", fg="white",
-       font=("Times New Roman", 15)).place(x=10, y=height_percent(85))
+
+Button(frame3, text="Play Again", command=reset, bg="#FDAE38", fg="#264248",
+       font=("Cabinet Grotesk", 15, "bold")).place(x=10, y=height_percent(85))
 
 
 window.mainloop()
